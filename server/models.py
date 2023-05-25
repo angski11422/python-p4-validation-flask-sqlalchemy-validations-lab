6b845774ce03d17ade96ff86594abbe5331d66fa
Angela Palaszewski <delarosa.ang@gmail.com>
2023-05-25 16:49:04 -0600

    complete lab

--- server/models.py
+++ server/models.py
@@ -5,6 +5,19 @@ db = SQLAlchemy()
 class Author(db.Model):
     __tablename__ = 'authors'
     # Add validations and constraints 
+    @validates('name')
+    def validate_name(self, key, value):
+        if value == '':
+            raise ValueError("Failed to validate")
+        return value
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if len(value) == 10:
+            return value
+        else:
+            raise ValueError("Failed to validate")
+
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
@@ -18,6 +31,31 @@ class Author(db.Model):
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
+    @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Failed to validate")
+        return value
+    
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) >= 250:
+            raise ValueError("Failed to validate")
+        return value
+    
+    @validates('category')
+    def validate_category(self, key, value):
+        if value == "Fiction" or value == "Non-Fiction":
+            return value
+        else:
+            raise ValueError("Failed to validate")
+
+    @validates('title')
+    def validate_title(self, key, value):
+        if "Won`t Believe" or "Secret" or "Top" or "Guess" not in value:
+            raise ValueError("Failed to validate")
+        return value
+
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
